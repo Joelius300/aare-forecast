79b9dccdeb9f4bb54fae55e751487f735f48fdcf
Joel L. <joelius300@protonmail.com>
2025-03-29 20:45:04 +0100

    things and stuff

--- aare/constants.py
+++ aare/constants.py
@@ -1,9 +1,14 @@
 # Location codes
 LOC_BERN = 2135
 LOC_THUN = 2030
+LOC_INT = 2457
 LOC_BERN_SMN = "BER"
 LOC_THUN_SMN = "THU"
+LOC_INT_SMN = "INT"
 
 # Column names
 TIME = "_time"
 TEMP = "temperature"
+
+ANYTIME = "0"
+"""To be used as period start when querying influx. Starting at 0 just returns all the data."""
--- aare/locations.py
+++ aare/locations.py
@@ -1,14 +1,14 @@
 import logging
 from typing import TypedDict, Literal, Optional
 
-from aare.constants import LOC_BERN, LOC_BERN_SMN, LOC_THUN, LOC_THUN_SMN
+from aare.constants import LOC_BERN, LOC_BERN_SMN, LOC_THUN, LOC_THUN_SMN, LOC_INT, LOC_INT_SMN
 
 logger = logging.getLogger(__name__)
 
 
 class LocationIds(TypedDict):
-    hydro: int
-    smn: str
+    hydro: int | None
+    smn: str | None
 
 
 LOC_ALIAS: dict[str, LocationIds] = {
@@ -20,6 +20,10 @@ LOC_ALIAS: dict[str, LocationIds] = {
         "hydro": LOC_THUN,
         "smn": LOC_THUN_SMN,
     },
+    "INT": {
+        "hydro": LOC_INT,
+        "smn": LOC_INT_SMN,
+    },
 }
 
 
@@ -29,10 +33,15 @@ def translate_location(loc: str | int, measurement: Optional[Literal["hydro", "s
 
     "BERN" -> "BER" or "2135" (depending on measurement). 2135 (int) -> "2135".
     """
+    assert measurement in ["smn", "hydro"], f"bad measurement: '{measurement}'"
     loc = str(loc).upper()
     if loc in LOC_ALIAS:
         if measurement:
-            return str(LOC_ALIAS[loc][measurement])
+            val = LOC_ALIAS[loc][measurement]
+            if val is not None:
+                return str(val)
+
+            raise ValueError(f"No measurement '{measurement}' for location '{loc}'")
         else:
             raise ValueError(f"Location Alias '{loc}' was passed, but no measurement (hydro or smn) was specified.")
 

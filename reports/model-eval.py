fd32ee484df1e02353affd75c0d4718c2e8b11eb
Joel L. <joelius300@protonmail.com>
2025-12-26 00:25:52 +0100

    fancy report

--- reports/model-eval.py
+++ reports/model-eval.py
@@ -5,12 +5,21 @@ app = marimo.App(width="medium", app_title="Aare Oraku Model Eval")
 
 
 @app.cell
-def _():
-    # TODO: add interactive chart with slider to slide through all available forecasts, to kind of get a feel for a model
+def _(mo):
+    mo.md(r"""
+    # Aare Oraku Model Evaluation
+
+    This notebook shows the accuracy of different Aare Oraku models.
+
+    Use the dropdown to select which model to evaluate. Use the sliders to tune how the models are evaluated, respectively which forecasts or parts of forecasts are considered when calculating the metrics. Most users of aare.guru will only look at the forecasts during the daytime in summer. The forecast horizon(s) people are interested probably depends on many factors; with the slider you can evaluate different views. Beware that you can introduce biases, especially when selecting very strict evaluation criteria.
+
+    TODO more notes on how this all works, caveats, what data, etc.
+    TODO just recipe for building and publishing report
+    """)
     return
 
 
-@app.cell
+@app.cell(hide_code=True)
 def _(default_model, mo, model_names):
     model_select = mo.ui.dropdown(options=model_names, value=default_model, label="Model to evaluate")
     model_select
@@ -41,10 +50,10 @@ def _(mo):
 
 
 @app.cell(hide_code=True)
-def _(horizon_range, hour_range, mo, model_metrics, month_range):
+def _(horizon_range, hour_range, mo, model, model_metrics, month_range):
     months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     mo.md(
-        f"Only taking into account forecasts for the months **{months[month_range.value[0] - 1]} - {months[month_range.value[1] - 1]}** and hours **{hour_range.value[0]:02d}:00 - {hour_range.value[1]:02d}:00**, and only looking at predictions made **{horizon_range.value[0]} - {horizon_range.value[1]} hours** into the future, the absolute errors across all steps (hours) are averaged.\n\nThe median forecast has a mean absolute error (MAE) of **{model_metrics['mae']:.3f} °C**. Across the entire validation series, 75% of forecasts have a MAE of {model_metrics['mae_q75']:.3f} °C or less, and 95% have a MAE of {model_metrics['mae_q95']:.3f} °C or less.  \nIf only the maximum temperature each day is relevant (regardless of timing), then the median forecast is off by **{model_metrics['madpd']:.3f} °C.** 75% of all forecasts have a daily peak difference of {model_metrics['madpd_q75']:.3f} °C or less and 95% have a DPD of {model_metrics['madpd_q95']:.3f} °C or less."
+        f"## Evaluation of {model} model\n\nOnly taking into account forecasts for the months **{months[month_range.value[0] - 1]} - {months[month_range.value[1] - 1]}** and hours **{hour_range.value[0]:02d}:00 - {hour_range.value[1]:02d}:00**, and only looking at predictions made **{horizon_range.value[0]} - {horizon_range.value[1]} hours** into the future, the absolute errors across all steps (hours) are averaged.\n\nThe median forecast has a mean absolute error (MAE) of **{model_metrics['mae']:.3f} °C**. Across the entire validation series, 75% of forecasts have a MAE of {model_metrics['mae_q75']:.3f} °C or less, and 95% have a MAE of {model_metrics['mae_q95']:.3f} °C or less.  \nIf only the maximum temperature each day is relevant (regardless of timing), then the median forecast is off by **{model_metrics['madpd']:.3f} °C.** 75% of all forecasts have a daily peak difference of {model_metrics['madpd_q75']:.3f} °C or less and 95% have a DPD of {model_metrics['madpd_q95']:.3f} °C or less."
     ).callout("success")
     return
 
@@ -70,11 +79,12 @@ def _(
             _col.abs().median(),
             _col.abs().quantile(0.125).name.suffix("_q12.5"),
             _col.abs().quantile(0.875).name.suffix("_q87.5"),
-            # pl.col("start_time", "end_time").unique().item(),  # this would be better, but needs polars >=1.35.0
+            # item instead of first would be better, but needs polars >=1.35.0 and pyodide is behind
             pl.col("start_time", "end_time").unique().first(),
         )
         .sort("run_ts")
-        .with_columns(cs.float().rolling_mean(30 * 24 // 5, center=True), valid=month_filter)
+        # smooth all runs in a week, at least 24 runs (all of one day). For this to work, stride must be 1!
+        .with_columns(cs.float().rolling_mean(7 * 24, center=True, min_samples=24), valid=month_filter)
         .collect()
     )
 
@@ -129,6 +139,56 @@ def _(px, run_metrics):
 
 
 @app.cell
+def _(
+    add_ignored_rects,
+    all_run_ts,
+    fc_i,
+    get_invalid_periods,
+    horizon_filter,
+    hour_filter,
+    month_filter,
+    pl,
+    px,
+    raw_metrics,
+):
+    _df = raw_metrics.filter(fc_i=fc_i.value).with_columns(valid=month_filter & hour_filter & horizon_filter)
+    _metrics = _df.filter("valid").select(pl.mean("ae", "adpd").mean().name.prefix("m")).to_dicts()[0]
+    _metrics_fmt = {k: (f"{v:.3f}" if v is not None else "-") for k, v in _metrics.items()}
+
+    _fig = px.line(
+        _df,
+        x="time",
+        y=["pred", "actual"],
+        color_discrete_sequence=["#73bf69", "#5694f2"],
+        title=f"Forecast from {all_run_ts[fc_i.value]} | MAE: {_metrics_fmt['mae']} | MADPD: {_metrics_fmt['madpd']}",
+    )
+
+    _invalid_periods = get_invalid_periods(_df, "time")
+    add_ignored_rects(_fig, _invalid_periods)
+
+    _fig
+    return
+
+
+@app.cell
+def _(all_run_ts, fc_i, mo):
+    mo.md(rf"""
+    Evaluation range: {all_run_ts[0]} - {all_run_ts[-1]}
+
+    Currently displaying: {all_run_ts[fc_i.value]}
+    """)
+    return
+
+
+@app.cell
+def _(mo, pl, raw_metrics):
+    all_run_ts = raw_metrics.select(pl.col("run_ts").unique(maintain_order=True)).to_series().to_list()
+    fc_i = mo.ui.slider(0, raw_metrics.select(pl.max("fc_i")).item(), step=1, full_width=True)
+    fc_i
+    return all_run_ts, fc_i
+
+
+@app.cell
 async def _():
     import sys
 
@@ -137,9 +197,9 @@ async def _():
     if running_wasm:
         import micropip
 
-        await micropip.install("tzdata")
-        await micropip.install("plotly[express]")
-        await micropip.install("polars")
+        await micropip.install("tzdata")  # needed for timezones to work
+        await micropip.install("plotly[express]")  # needed to override wrongly installed plotly-express
+        await micropip.install("polars")  # just to be sure, would probably be installed regardless
     return (running_wasm,)
 
 
@@ -187,7 +247,9 @@ def _(mo, model, running_wasm):
         if running_wasm:
             return str(notebook_loc / "public" / f"{model}.csv") if notebook_loc is not None else None
 
-        # avoid importing aare.paths, because it will then try to fetch it from pypi, which is a security risk
+        # avoid importing aare.paths, because it will then try to fetch it from pypi, which is a security risk.
+        # note that it already installs the super outdated plotly-express and tries to install plotly-graph-objects and polars-selectors!!
+        # https://github.com/marimo-team/marimo/issues/6366#issuecomment-3691495696
         metrics_folder = notebook_loc.parent / "data" / "metrics"
         path = metrics_folder / "raw" / f"{model}.csv"
         return str(path) if path.exists() else None
@@ -204,14 +266,18 @@ def _(data_path, mo, pl, timedelta, tz):
     def load_raw(path: str, tz: str) -> pl.DataFrame:
         raw_metrics = pl.read_csv(data_path)
 
-        # parse timestamps
-        raw_metrics = raw_metrics.with_columns(pl.col("run_ts", "time").str.to_datetime(time_zone=tz))
-        # add lag (horizon) column
-        raw_metrics = raw_metrics.with_columns(
-            lag=((pl.col("time") - pl.col("run_ts")) / timedelta(hours=1) + 1).cast(int)
+        raw_metrics = (
+            raw_metrics.lazy()
+            # parse timestamps
+            .with_columns(pl.col("run_ts", "time").str.to_datetime(time_zone=tz))
+            # add lag column
+            .with_columns(lag=((pl.col("time") - pl.col("run_ts")) / timedelta(hours=1) + 1).cast(int))
+            # add absolute error columns
+            .with_columns(ae=pl.col("err").abs(), adpd=pl.col("dpd").abs())
+            # add unique id for each forecast
+            .with_columns(pl.col("run_ts").rle_id().alias("fc_i"))
+            .collect()
         )
-        # add error columns
-        raw_metrics = raw_metrics.with_columns(ae=pl.col("err").abs(), adpd=pl.col("dpd").abs())
 
         # add start and end time for each forecast run
         raw_metrics = raw_metrics.join(
@@ -258,7 +324,7 @@ def _(cs, filtered_all, pl):
 
 
 @app.cell
-def _(go, pl):
+def _(add_ignored_rects, get_invalid_periods, go, pl):
     def quantile_line_chart(
         df: pl.DataFrame,
         x_col="run_ts",
@@ -321,30 +387,8 @@ def _(go, pl):
         )
 
         if "valid" in df.columns:
-            invalid_periods = (
-                df.lazy()
-                .with_columns(blackout=~pl.col("valid"))
-                .with_columns(group_id=pl.col("blackout").rle_id())
-                .filter("blackout")
-                .group_by("group_id")
-                .agg(
-                    start=pl.min(x_col),
-                    end=pl.max(x_col),
-                )
-                .drop("group_id")
-                .collect()
-            )
-
-            for start, end in invalid_periods.iter_rows():
-                fig.add_vrect(
-                    start,
-                    end,
-                    line_width=0,
-                    fillcolor="darkgrey",
-                    opacity=0.2,
-                    annotation_text="ignored",
-                    annotation_position="top left",
-                )
+            invalid_periods = get_invalid_periods(df, x_col)
+            add_ignored_rects(fig, invalid_periods)
 
         return fig
 
@@ -356,6 +400,47 @@ def _():
     import marimo as mo
 
     return (mo,)
+
+
+@app.cell
+def _(pl):
+    def get_invalid_periods(df: pl.DataFrame, index_col: str, valid_col="valid") -> pl.DataFrame:
+        """Get start and end tuples for 'invalid' periods (~valid)."""
+        return (
+            df.lazy()
+            .with_columns(blackout=~pl.col(valid_col))
+            .with_columns(group_id=pl.col("blackout").rle_id())
+            .filter("blackout")
+            .group_by("group_id")
+            .agg(
+                start=pl.min(index_col),
+                end=pl.max(index_col),
+            )
+            .drop("group_id")
+            .collect()
+        )
+
+    return (get_invalid_periods,)
+
+
+@app.cell
+def _(go, pl):
+    def add_ignored_rects(fig: go.Figure, invalid_periods: pl.DataFrame):
+        for start, end in invalid_periods.iter_rows():
+            fig.add_vrect(
+                start,
+                end,
+                line_width=0,
+                fillcolor="darkgrey",
+                opacity=0.2,
+                annotation_text="ignored",
+                annotation_position="top left",
+            )
+
+        # marimo x plotly update logic is fucked and sometimes annotations are left over and fuck up the chart.
+        # trying to remove those leftover annotations here doesn't work because they aren't in the fig object at all...
+
+    return (add_ignored_rects,)
 
 
 if __name__ == "__main__":

996fdebefe20d8dc2528e1828fb156ddce39d770
Joel L. <joelius300@protonmail.com>
2026-05-16 21:27:41 +0200

    update model docs and report

--- docs/models.md
+++ docs/models.md
@@ -11,9 +11,15 @@ single use case with just one location, so a lot can and will change when other 
 
 ## Nowcasting temp[erature]
 
-This model use case is for short-term river temperature forecasts (1-2 days).
+This model use case is for short-term river temperature forecasts (1-2 days) with 1h resolution.
 It's the first and only model at the moment, but there are plans to make models for longer horizons
 and potentially even river flow.
+
+> [!IMPORTANT]
+> "nowcasting" isn't a good name for this. I had naively thought that the water temperature moves so slowly that 1h
+> would be the highest resolution we need but closer examination of live forecasts in early summer with volatile weather
+> has shown that actual nowcasting e.g. 10min resolution for the next 3-6 hours might have its uses too.
+> If I ever implement actual nowcasting, this model might be renamed to shortcasting or something similar.
 
 ### v1.0
 
@@ -23,5 +29,32 @@ Good enough for a first model, but definitely has lots of room for improvement.
 - **Inputs features:** Air temperature (with non-linear transforms) at lags up to 24h
 - **Locations:** Only Bern, Schönau (2135)
 - **Accuracy:** 50% of forecasts have a mean absolute error across all the predicted 36h of **0.4 °C or less**.
-  **80% of 0.7 °C or less**. In the worst summer periods, errors are regularly between 0.5 and 1.5 °C at larger lags.
+  **80% of 0.72 °C or less**. For just the first hour, 75% are below 0.23°C.
+  In the worst summer periods, errors are regularly between 0.5 and 1.5 °C at larger lags.
   _This is only on measurements, performance will degrade when using forecasts as inputs (like during inference)!_
+
+### v1.1
+
+> [!IMPORTANT]
+> This version is released together with a change in the target variable. The switch from hourly mean to point in time
+> forecasts was made for better alignment with the use case and visualization at aare.guru.
+
+Same as v1.0, with the following differences:
+
+- temp_bern diff_threshold (used for outlier detection) increased from 0.6°C to 1.0°C to handle volatile spring/early
+  summer weather.
+- switch from hourly mean values to subsampling to avoid a target and covariate mismatch when a) plotting forecasts
+  alongside point in time measurement values and b) using meteotest forecasts as inputs, which are also aligned
+  to points in time without mean aggregation.
+
+These two changes improved the accuracy of the model as well.
+
+- 50% of forecasts have a mean absolute error across all the predicted 36h of **0.38 °C or less**.
+  **80% of 0.69 °C or less**.
+- For just the first hour, 75% are below 0.13°C (this is the biggest win of this update).
+
+### v1.2
+
+Same as v1.1, with the following differences:
+
+- update to darts v0.44.1
--- reports/model-eval.py
+++ reports/model-eval.py
@@ -10,7 +10,7 @@
 
 import marimo
 
-__generated_with = "0.20.4"
+__generated_with = "0.23.6"
 app = marimo.App(width="medium", app_title="Aare Oraku Model Evaluation")
 
 
@@ -21,34 +21,26 @@ def _(mo):
 
     This notebook shows the accuracy of different Aare Oraku models and allows comparisons with baselines.
 
-    Use the dropdown to select which model to evaluate. Use the sliders to tune how the models are evaluated, respectively which forecasts or parts of forecasts are considered when calculating the metrics. 
+    Use the dropdown to select which model to evaluate. Use the sliders to tune how the models are evaluated, respectively which forecasts or parts of forecasts are considered when calculating the metrics.
     Most users of aare.guru will only look at the forecasts during the daytime in summer.
     Which forecast horizon (how many hours into the future) people are interested in probably depends on many factors; with the slider you can evaluate different views.
     Beware that you can introduce biases, especially when selecting very strict evaluation criteria.
 
-    It's important to note that this evaluation is very optimistic because all models that use external data as input like air temperature are evaluated on true measurement data. 
-    During inference (on aare.guru), this external data comes from forecasting services like MeteoTest, so it will contain inaccuracies that are propagated to our models. 
+    It's important to note that this evaluation is very optimistic because all models that use external data as input like air temperature are evaluated on true measurement data.
+    During inference (on aare.guru), this external data comes from forecasting services like MeteoTest, so it will contain inaccuracies that are propagated to our models.
     How well the model performs with external forecast inputs we can't know yet, but it's very likely that it will be worse than this evaluation shows.
     How much worse it will be depends on the accuracy of the external forecast services and sensitivity of our model.
     A [preliminary analysis of real forecasts](https://github.com/Joelius300/aare-forecast/blob/main/notebooks/21_measurement-vs-forecast-eval.ipynb) showed that they might be 15-25% worse than test forecasts of the same model
     during the same period. This is for the full 4-day forecasts, restricting evaluation to just the data shown in the
     aare.guru main page, it drops to ~10%. Relative to the actual temperature, it's only ~1% worse on average.
-    
+
     If you uncheck the test dataset and instead look at the validation data, the evaluation will be even more optimistic because the validation set is used to tune the model and select the best one, which introduces a bias.
     The test set is designed to be evaluated only once a model is tuned and selected to avoid such biases and get the most realistic estimate for the real-world model accuracy.
     Luckily, the simple models used in the MVP are not overtuned and show very low differences between validation and test set.
-    
+
     To make sure the Aare Oraku forecasts are accurate enough, they are continually monitored and evaluated.
     At the same time, all historical forecasts including all external inputs are stored for future evaluation.
     """)
-    return
-
-
-@app.cell
-def _():
-    # TODO more notes on how this all works, caveats, what data, etc.
-    # TODO just recipe for building and publishing report
-    # TODO also publish json meta files for the models so we can display the features it uses
     return
 
 
@@ -65,6 +57,10 @@ def _(mo, model):
     if model == "LR-dev-live-proto":
         _out = mo.md(
             "This model is special and you probably don't care for it. It was the prototype model that ran with real forecast data. When the test data checkbox is checked, you can analyze the evaluation with **measured** air temperature. If you uncheck it, it will _not_ use validation data, but instead it will show real historical forecast made with **forecasted** air temperature. This allows comparison between real historical forecasts and simulated forecasts on test data for the same time period."
+        ).callout("info")
+    elif model == "nowcasting_temp-1.0":
+        _out = mo.md(
+            "The 1.0 model was trained with hourly mean water temperature as target and is therefore systematically misaligned. The evaluation was retroactively corrected to use the new target (point in time measurements) for error calculation."
         ).callout("info")
 
     _out
@@ -250,6 +246,8 @@ def _(
         color_discrete_sequence=["#73bf69", "#5694f2"],
         title=f"Forecast from {all_run_ts[fc_i.value]} | MAE: {_metrics_fmt['mae']} | MADPD: {_metrics_fmt['madpd']}",
     )
+    _fig.update_yaxes(title_text="Water temperature [°C]")
+    _fig.update_xaxes(title=None)
 
     _invalid_periods = get_invalid_periods(_df, "time")
     add_ignored_rects(_fig, _invalid_periods)
@@ -344,9 +342,7 @@ def _(itertools):
     tz = "Europe/Zurich"
     # todo if not running wasm, populate this list with files from disk?
     models = {
-        "nowcasting_temp": [
-            "1.0",
-        ],
+        "nowcasting_temp": ["1.0", "1.2"],
         "LR-dev": [
             "live-proto",
         ],
@@ -358,7 +354,7 @@ def _(itertools):
         for model_key, versions in models.items()
         for model, version in zip(itertools.repeat(model_key), versions)
     ] + baseline_models
-    default_model = "nowcasting_temp-1.0"
+    default_model = "nowcasting_temp-1.2"
     return baseline_models, default_model, model_names, tz
 
 
@@ -559,7 +555,6 @@ def _(go, pl):
 
         # marimo x plotly update logic is fucked and sometimes annotations are left over and fuck up the chart.
         # trying to remove those leftover annotations here doesn't work because they aren't in the fig object at all...
-
     return (add_ignored_rects,)
 
 
@@ -611,6 +606,7 @@ def _(mo, model, pathlib, running_wasm, test_checkbox):
 
         return notebook_loc.parent / "data"
 
+
     def get_data_path() -> str | None:
         file_name = model + ("-test" if test_checkbox.value else "") + ".parquet"
         asset_dir = get_assets_dir()
@@ -622,6 +618,7 @@ def _(mo, model, pathlib, running_wasm, test_checkbox):
 
         path = asset_dir / "metrics" / "raw" / file_name
         return str(path) if path.exists() else None
+
 
     def get_meta_path() -> str | None:
         file_name = model + ".json"
